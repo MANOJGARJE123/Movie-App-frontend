de6cfc6c1492b37fd3eef87192317a0902b93c45
Manoj Garje <manojgarje123@gmail.com>
2026-01-27 17:56:57 +0530

    Adds(feature): Api js

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,10 @@ const BASE_URL = process.env.BASE_URL;
 
 export const fetchMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json();
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query) }`)
 }
