f12f57617f6f2cf91281a429095a9af6a4cbc85e
Manoj Garje <manojgarje123@gmail.com>
2026-01-28 16:32:13 +0530

    Adds : Fav movies collection feature

--- src/page/Favorites.jsx
+++ src/page/Favorites.jsx
@@ -1,7 +1,26 @@
 import "../css/Favorites.css"
+import "../css/Home.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
 
 function Favorite() {
 
+    const { favorites } = useMovieContext();
+
+    if (favorites.length > 0) {
+        return (
+        <div>
+            <div>
+                <h2>Your Favorites</h2>
+            </div>
+            <div className="movies-grid">
+                {favorites.map((movie, idx) => (
+                    <MovieCard key={movie.id ?? `${movie.title}-${movie.release_date}-${idx}`} movie={movie} />
+                ))}
+            </div>
+        </div>
+        )
+    }
     return <div className="favorites-empty">
         <h2>Your Favorites List is Empty</h2>
         <p>Add movies to your favorites to see them here.</p>
